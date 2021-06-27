fbfab7ff4311c94de397f6aad9927bb8e19998c7
Oscar Tellez <oscartzgz@gmail.com>
2021-06-27 07:33:09 -0400

    available images with asset_pack_tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// To access by assetp_pack_tag
+function importAll(r) {
+ r.keys().forEach(r);
+}
+// Add relevant file extensions as needed below.
+// I'm sure there is a better way :shrug:
+importAll(require.context('../images/', true, /\.(svg|jpg|png)$/));
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
