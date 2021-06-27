81314207f6ce445ada09d17034ccd9e3cf5504dd
Oscar Tellez <oscartzgz@gmail.com>
2021-06-26 21:52:24 -0400

    Setup devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create_with(password: "Morgado12$").find_or_create_by(email: "admin@mmorgado.com")
