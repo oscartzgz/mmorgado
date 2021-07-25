18bcc34494b7b38ed55584c20200c59cf0f48eb1
Oscar Tellez <oscartzgz@gmail.com>
2021-07-24 21:15:35 -0400

    Setup devise

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'slim-rails'
+gem 'devise'
 
 gem 'capistrano', '~> 3.11'
 gem 'capistrano-rails', '~> 1.4'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create_with(username: 'admin', password: "Morgado12$").find_or_create_by(email: "admin@mmorgado.com")
