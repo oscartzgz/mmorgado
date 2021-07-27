b795a9971268adddcd72063ccad6cb6912df36e7
Oscar Tellez <oscartzgz@gmail.com>
2021-07-27 09:40:17 -0400

    Enable migrations on deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,6 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-puts 'Esto tiene deploy_host: ', ENV['DEPLOY_HOST']
 server ENV['DEPLOY_HOST'], user: ENV['DEPLOY_USER'], roles: %w{app db web}
 
 
