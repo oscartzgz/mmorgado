b4b589c9672ca7f5a540b339c4a354baba7a9f04
Oscar Tellez <oscartzgz@gmail.com>
2021-08-11 06:27:49 -0400

    Add some translations

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ApplicationRecord
   def full_name
     [name, first_surname, second_surname].join(' ')
   end
+
+  def full_address
+    [address, location, state].join(' ')
+  end
 end
