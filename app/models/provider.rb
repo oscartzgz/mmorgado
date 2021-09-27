e7fac12671c0e06cd040071563bacc43f815bc47
Oscar Tellez <oscartzgz@gmail.com>
2021-09-26 22:45:54 -0500

    Add validations to provider

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,6 +3,9 @@ class Provider < ApplicationRecord
   has_many :price_formulas
   has_many :product_brands, through: :products
 
+  validates_presence_of :full_name, :address, :contact_full_name,
+                        :phone_number, :contact_mobile_number
+
   after_create do
     PriceFormulaUpdater.new(self).update
   end
